339ec9389919d63f7aed515676b551060dcaefef
Gary <Gary@DESKTOP-F8U2KJV>
2025-02-26 08:19:49 +1100

    Add project files.

---
+++ Restaurants.Infrastructure/Persistence/RestaurantDbContext.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence
+{
+  internal  class RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : DbContext(options)
+    {
+        internal DbSet<Restaurant> Restaurants { get; set; }
+        internal DbSet<Dish> Dishes { get; set; }
+
+        //override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if(!optionsBuilder.IsConfigured)
+        //    {
+        //        optionsBuilder.UseSqlServer("Server=garynas224plus\\MSSQL-SERVER,1433;Database=RestaurantsDb;User=sa;Password=DataSeasoning09021989;TrustServerCertificate=True", optionsBuilder => optionsBuilder.EnableRetryOnFailure());   
+        //    }
+               
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.HasDefaultSchema("TutorialAppSchema");
+            //modelBuilder.Entity<User>().ToTable("Users", "TutorialAppSchema").HasKey(u => u.UserId);
+            //modelBuilder.Entity<UserSalary>().HasKey(u => u.UserId);
+            //modelBuilder.Entity<UserJobInfo>().HasKey(u => u.UserId);
+
+            base.OnModelCreating(modelBuilder); //this will create the tables
+            modelBuilder.Entity<Restaurant>().OwnsOne(r => r.Address);
+            modelBuilder.Entity<Restaurant>().HasMany(r => r.Dishes).WithOne().HasForeignKey(d => d.RestaurantId);
+        }
+    }
+}
---
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -0,0 +1,90 @@
+﻿using Restaurants.Domain.Entities;
+using Restaurants.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Seeders
+{
+    internal class RestaurantSeeder(RestaurantDbContext dbContext) : IRestaurantSeeder
+    {
+        public async Task Seed()
+        {
+            if (await dbContext.Database.CanConnectAsync())
+            {
+                if (!dbContext.Restaurants.Any())
+                {
+                    var restaurant = GetRestaurants();
+                    dbContext.Restaurants.AddRange(restaurant);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            List<Restaurant> restaurants = [
+                new Restaurant()
+                {
+                    Name = "McDonalds",
+                    Description = "Fast food restaurant",
+                    Category = "Fast Food",
+                    HasDelivery = true,
+                    ContactEmail = "contact@mcdonalds.com",
+                    Dishes = [
+                        new()
+                        {
+                            Name = "Big Mac",
+                            Description = "Tasty burger",
+                            Price = 5.99m
+                        },
+                         new()
+                        {
+                            Name = "Big Mac",
+                            Description = "Tasty burger",
+                            Price = 5.99m
+                        }
+                        ],
+                    Address = new ()
+                    {
+                        City = "New York",
+                        Street = "Broadway",
+                        PostalCode = "10001"
+                    }
+                },
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Description = "Fast food restaurant",
+                    Category = "Fast Food",
+                    HasDelivery = true,
+                    ContactEmail = "contact@kfs@mcdonalds.com",
+                    Dishes = [
+                        new()
+                        {
+                            Name = "Chicken wings",
+                            Description = "Spicy chicken wings",
+                            Price = 10.99m
+                        },
+                         new()
+                        {
+                            Name = "Chicken wings",
+                            Description = "Spicy chicken wings",
+                            Price = 10.99m
+                        }
+                        ],
+                    Address = new()
+                    {
+                        City = "New York",
+                        Street = "Broadway",
+                        PostalCode = "10001"
+                    }
+                }
+                ];
+            return restaurants;
+        }
+    }
+}
